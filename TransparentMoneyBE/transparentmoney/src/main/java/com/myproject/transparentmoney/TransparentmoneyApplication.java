ffc6e0e3c9c6c55ac89a9a343bd3c21681debce6
Tung Chun Xian <tungchunxian@gmail.com>
2024-09-09 23:41:37 +0800

    Setup - FE and BE

---
+++ TransparentMoneyBE/transparentmoney/src/main/java/com/myproject/transparentmoney/TransparentmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.transparentmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparentmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransparentmoneyApplication.class, args);
+	}
+
+}
